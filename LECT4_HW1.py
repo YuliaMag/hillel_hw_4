92cd381668b63133180e23c6aaf6981ecd5dafa6
Yulia Magonya <ymagonia@gmail.com>
2024-04-23 23:33:24 +0300

    ---

--- LECT4_HW1.py
+++ LECT4_HW1.py
@@ -2,7 +2,7 @@
 # HW1.1
 inp_str = input("Input string: ")
 
-inp_str = inp_str.strip() .lower()
+inp_str = inp_str.strip().lower()
 rev_str = reversed(inp_str)
 
 if list(inp_str) == list(rev_str):
@@ -15,7 +15,7 @@ print("-------------------")
 # HW1.2
 
 n = input("Input string: ")
-nn = n.strip() .lower()
+nn = n.strip().lower()
 
 if nn == nn[::-1]:
     print(True)
