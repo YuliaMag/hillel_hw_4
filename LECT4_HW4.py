851212e8ae445ec52f172e2404312254569d8ba3
Yulia Magonya <ymagonia@gmail.com>
2024-04-25 00:43:30 +0300

    111111

--- LECT4_HW4.py
+++ LECT4_HW4.py
@@ -12,9 +12,20 @@ elif "@" in email[-1]:
     print(False)
 elif "." in email[-1]:
     print(False)
-#elif TODO
+elif email.count("@") > 1:
+    print(False)
+elif email.count(".") > 1:
+    print(False)
+elif email.find("@") > email.find("."):
+    print(False)
+elif not email.isalnum():
+    if email.__contains__("@") or email.__contains__("."):
+        print(True)
+    else:
+        print(False)
 else:
-    print(True)
+    print("True")
+
 
 
 
