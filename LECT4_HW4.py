602b96aab983f2a943b791cae80b11b875fb8304
Yulia Magonya <ymagonia@gmail.com>
2024-04-25 22:58:10 +0300

    email

--- LECT4_HW4.py
+++ LECT4_HW4.py
@@ -1,32 +1,46 @@
 
-email = str(input("Email Address: "))
+email = input("Email Address: ")
+
 if "@" not in email:
     print(False)
+# testatest.com
+
 elif "." not in email:
     print(False)
-elif "@" in email[0]:
+# test@testdotcom
+
+elif "@" == email[0]:
     print(False)
-elif "." in email[0]:
-    print(False)
-elif "@" in email[-1]:
-    print(False)
+# @test.test
+
 elif "." in email[-1]:
     print(False)
-elif email.count("@") > 1:
+# test@test.
+
+elif email.count("@") > 1: # if I put "!= 2" then 3 "@" will be valid input
     print(False)
-elif email.count(".") > 1:
+# test@test@test.com
+
+elif email.count(".") > 1: # if I put "!= 2" then 3 "." will be valid input
     print(False)
+# test@test.test.com
+
 elif email.find("@") > email.find("."):
     print(False)
+# test.test@com
+
 elif not email.isalnum():
-    # if email.__contains__("@") or email.__contains__("."):
     if email.find("@") or email.find("."):
-        print(True)
+        pass
     else:
         print(False)
+    print(False)
+# )*(^&*%^$%$##%^$^%
+# #$%@&*(.% -- I missed this one
+
 else:
     print("True")
-
+# test@test.com
 
 
 
