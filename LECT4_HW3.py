41d72c366f224837645bc2f86420c093c2b054f5
Yulia Magonya <ymagonia@gmail.com>
2024-04-25 21:38:54 +0300

    hw3_redo

--- LECT4_HW3.py
+++ LECT4_HW3.py
@@ -1,6 +1,6 @@
-
-string_1 = "This tool is cool. But that owl is awful. MAGIC TOOLS Ltd."
-
+#
+# string_1 = "This tool is cool. But that owl is awful. MAGIC TOOLS Ltd."
+#
 # list_1 = string_1.split()
 # for elem in list_1:
 #     if elem.__contains__("oo"):
@@ -8,10 +8,24 @@ string_1 = "This tool is cool. But that owl is awful. MAGIC TOOLS Ltd."
 #     elif elem.__contains__("OO"):
 #         print(elem.title())
 #
+#
+# list_1 = string_1.split()
+#
+# for elem in list_1:
+#     if elem.count("oo"):
+#         print(elem.title(), end=" ")
+#     elif elem.count("OO"):
+#         print(elem.title())
+#
+# for x in l_st:
+#     if x.count("o") or x.count("O"):
+#         print(x.title(), end=" ")
 
-list_1 = string_1.split()
-for elem in list_1:
-    if elem.count("oo"):
-        print(elem.title(), end=" ")
-    elif elem.count("OO"):
-        print(elem.title())
+
+string_1 = "This tool is cool. But that owl is awful. MAGIC TOOLS Ltd."
+
+l_st = string_1.split(" ")
+
+for x in l_st:
+    if x.count("o") == 2 or x.count("O") == 2:
+        print(x.title(), end=" ")
