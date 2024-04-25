aff5b86c011e6a90444f383a1b23fcaf43b378f0
Yulia Magonya <ymagonia@gmail.com>
2024-04-25 09:46:09 +0300

    correction hw4_3

--- LECT4_HW3.py
+++ LECT4_HW3.py
@@ -1,13 +1,17 @@
 
 string_1 = "This tool is cool. But that owl is awful. MAGIC TOOLS Ltd."
 
+# list_1 = string_1.split()
+# for elem in list_1:
+#     if elem.__contains__("oo"):
+#         print(elem.title(), end=" ")
+#     elif elem.__contains__("OO"):
+#         print(elem.title())
+#
+
 list_1 = string_1.split()
-
 for elem in list_1:
-    if elem.__contains__("oo"):
+    if elem.count("oo"):
         print(elem.title(), end=" ")
-    elif elem.__contains__("OO"):
+    elif elem.count("OO"):
         print(elem.title())
-
-
-
