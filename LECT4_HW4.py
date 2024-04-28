6b13eefa109b2f9e1bade8aaf20aa6f6062417f8
Yulia Magonya <ymagonia@gmail.com>
2024-04-28 12:48:26 +0300

    test_сщcorr

--- LECT4_HW4.py
+++ LECT4_HW4.py
@@ -4,8 +4,8 @@ s = "! # $ % ^ & * ( ) _ + = - / , < > ?"
 l_n = s.split(" ")
 
 for x in l_n:
-    if ((x in email) or (email.count("@") != 1 or email.count(".") != 1) or ("@" == email[0] or "." == email[-1]) or
-            (email.find("@") > email.find(".")) or (email.find(".") == email.find("@") + 1)):
+    if (x in email or email.count("@") != 1 or email.count(".") != 1 or "@" == email[0] or "." == email[-1] or
+            email.find("@") > email.find(".") or email.find(".") == email.find("@") + 1):
         print(False)
         break
     else:
