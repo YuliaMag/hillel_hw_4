3c74c7405589a77a544000507c38ad420ef32361
Yulia Magonya <ymagonia@gmail.com>
2024-04-29 01:33:04 +0300

    wwwwwwwww

--- LECT4_HW4.py
+++ LECT4_HW4.py
@@ -2,23 +2,27 @@ email = input("Email Address: ")
 q = 0
 w = 0
 prev_chr = ""
-
+s_end = len(email) - 1
 res = True
 
 for i in range(len(email)):
     if email[i].isalnum():
         prev_chr = email[i]
         continue
-    if email[i] == "@" and q == 0 and 0 < i < (len(email) - 1):
+    if email[i] == "@" and q == 0 and 0 < i < s_end:
         q = 1
         prev_chr = email[i]
         continue
-    if email[i] == "." and w == 0 and 0 < i < (len(email) - 1) and q == 1 and prev_chr != "@":
+    if email[i] == "." and w == 0 and 0 < i < s_end and q == 1 and prev_chr != "@":
         w = 1
         prev_chr = email[i]
         continue
     res = False
     break
+
+if q == 0 or w == 0:
+    res = False
+
 print(res)
 
 
